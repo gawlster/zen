ef8eb6c10b4e9f3782bcffad011fd59df6e7404f
gawlster <connorgawley@gmail.com>
2025-08-07 15:55:39 -0700

    Add manifest for standalone pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <title>Zen</title>
+                <link rel="manifest" href="manifest.json"></link>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
